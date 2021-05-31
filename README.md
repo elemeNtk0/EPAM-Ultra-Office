afba411d1647cbcc53e956c57ed906d9fe62b7d4
elemeNtk0 <elemeNtk0gm@gmail.com>
2021-06-01 01:23:41 +0300

    update readme

--- README.md
+++ README.md
@@ -5,8 +5,14 @@ Learning process in EPAM
 ## 01_basics:
 Basic concepts of Javascript language.
 
+-----
 ### Вопросы:
   - Как часто вообще приходится работать с `parseInt / parseFloat`?
   - Почему `isFinite(1/0) // false`, а `1 / 0; // Infinity`? 
   - Почему `array.length` возвращает самый большой индекс + 1. т.е. 0 длина массива никогда не может быть? 
   - Как быть с [собственными обьектами](https://developer.mozilla.org/ru/docs/Web/JavaScript/A_re-introduction_to_JavaScript#%D1%81%D0%BE%D0%B1%D1%81%D1%82%D0%B2%D0%B5%D0%BD%D0%BD%D1%8B%D0%B5_%D0%BE%D0%B1%D1%8A%D0%B5%D0%BA%D1%82%D1%8B)? Я не понимаю элементарного — почему мы в простых примерах присваиваем `this.first = first` и подобные вещи. Для чего это переприсвоение идёт? Почему нельзя просто написать `return first`? По доке оч сложно разрулить…
+
+-----
+### Задачи:
+  [x] Ex.1 
+  [ ] Ex.2 — filterNonUnique догадки — надо фильтровать, но как не понимаю. Ищу примеры, ини либо с использованием `Set`, либо с ним и ещё с `Reduce`.
--- js-basic-course/modules/01_basics/index.js
+++ js-basic-course/modules/01_basics/index.js
@@ -1,5 +1,8 @@
 /**
- * Change the capitalization of all letters in a string
+ * Ex.1: Change the capitalization of all letters in a string
+ *
+ * Проблема возникла в том, что не мог сначала понять — почему не записывается значение.
+ * Пришлось добавить вторую переменную, чтобы записывать результат в неё (transformedText)
  */
 const changeCase = (text) => {
   let textArray = text.split(''),
@@ -18,6 +21,25 @@ console.log(changeCase('21century'));
 console.log(changeCase('Hybris'));
 
 /**
- * Проблема возникла в том, что не мог сначала понять — почему не записывается значение.
- * Пришлось добавить вторую переменную, чтобы записывать результат в неё (transformedText)
+ * Ex.2: Filter out the non-unique values in an array
+ * Понял, что надо сравнивать каждый с каждым, но первое решение вообще меня запутало.
+ * Решил поискать примеры. Наткнулся на https://learn.javascript.ru/task/array-unique.
+ * Надо как-то фильтровать, но я не понимаю по какой логике — фор внутри фора ерунда. 
+ * Не выходит...
  */
+const filterNonUnique = (array) => {
+  let resultArray = [];
+
+  for (let item of array) {
+    if (!resultArray.includes(item)) {
+      resultArray.push(item);
+    } else {
+      // resultArray.remove(item);
+    }
+  }
+
+  return resultArray;
+};
+
+console.log(filterNonUnique([1, 2, 2, 3, 4, 4, 5])); // Output: [1,3,5]
+console.log(filterNonUnique([1, 2, 3, 4])); // Output: [1,2,3,4]
